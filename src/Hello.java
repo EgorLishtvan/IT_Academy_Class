f6df2e7009c30667e39cf080f90e3d9f20339e2b
Egor Lishtvan <Lishtvan>
2022-02-17 23:54:02 +0300

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+
+
+public class Hello {
+
+
+    public static void main(String[] args) {
+        String nameOfTheMan = "Ivan";
+        int height = 176;
+        System.out.println("Hello " + nameOfTheMan);
+        System.out.println(height);
+
+    }
+}
